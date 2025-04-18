d028c34877c657c7df41b649e27efa4661d76b8d
Deavanzack <daffafadillah564@gmail.com>
2025-04-19 02:20:07 +0700

    menyederhanakan function perhitungan

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -92,15 +92,19 @@ public class Employee {
 	
 	public int getAnnualIncomeTax() {
 		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+		int monthsWorked = joinDate.getYear() == LocalDate.now().getYear()
+        ? LocalDate.now().getMonthValue() - joinDate.getMonthValue()
+        : 12;
+
+    	boolean isSingle = spouseIdNumber == null || spouseIdNumber.isEmpty();
+
+    	return TaxFunction.calculateTax(
+        	monthlySalary,
+        	additionalIncome,
+        	monthsWorked,
+        	annualDeductible,
+        	isSingle,
+        	childIdNumbers.size()
+    	);
 	}
 }
